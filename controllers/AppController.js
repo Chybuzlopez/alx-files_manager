cb98a9c96159a5fa65e823d75474c0b6585aa9da
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-25 21:41:03 +0000

    app controller

---
+++ controllers/AppController.js
@@ -0,0 +1,22 @@
+import RedisClient from '../utils/redis';
+import DBClient from '../utils/db';
+
+class AppController {
+  static getStatus(req, res) {
+    const status = {
+      redis: RedisClient.isAlive(),
+      db: DBClient.isAlive(),
+    };
+    return res.status(200).send(status);
+  }
+
+  static async getStats(req, res) {
+    const stats = {
+      users: await DBClient.nbUsers(),
+      files: await DBClient.nbFiles(),
+    };
+    return res.status(200).send(stats);
+  }
+}
+
+module.exports = AppController;
