ef7f40f315bb11136d73a8e4bd8f6bc38f9a600b
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-28 09:05:14 +0000

    added 3 endpoints to authenticate user

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 
 import { getStatus, getStats } from '../controllers/AppController';
+import { postNew } from '../controllers/UsersController';
+import { getConnect, getDisconnect } from '../controllers/AuthController';
+import { getMe } from '../controllers/UserController';
 
 const router = express.Router();
 
 router.get('/status', getStatus);
 router.get('/stats', getStats);
 router.post('/users', postNew);
+router.get('/connect', getConnect);
+router.get('/disconnect', getDisconnect);
+router.get('users/:me', getMe);
 
 export default router;
