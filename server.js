5813ade095b5409ddd8064776cc7475c6d9d12f5
Aniebiet Afia <54312052+AfiaAniebiet@users.noreply.github.com>
2023-06-29 22:22:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import router from './routes/index';
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(router);
 
 const port = process.env.PORT || 5000;
