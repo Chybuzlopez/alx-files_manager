941fe825d6f4b43701abb8d43e1085912305a580
Aniebiet Afia <54312052+AfiaAniebiet@users.noreply.github.com>
2023-06-29 22:07:30 +0100

    Update redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -1,31 +1,25 @@
-import redis from 'redis';
-import { promisify } from 'util';
+const redis = require('redis');
+const { promisify } = require('util');
 
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
-    this.getClient = promisify(this.client.get).bind(this.client);
-    this.client.on('error', (error) => {
-      console.log(`Redis client not connected to the server: ${error.message}`);
-    });
   }
-
   isAlive() {
     return this.client.connected;
   }
-
   async get(key) {
-    const value = await this.getClient(key);
+    const getAsnc = promisify(this.client.get).bind(this.client);
+    const value = await getAsnc(key);
     return value;
   }
-
   async set(key, value, duration) {
-    this.client.setex(key, duration, value);
+    await this.client.set(key, value);
+    await this.client.expire(key, duration);
   }
-
   async del(key) {
-    this.client.del(key);
+    await this.client.del(key);
   }
 }
 const redisClient = new RedisClient();
-export default redisClient;
+module.exports = redisClient;
